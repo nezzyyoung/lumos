3f87a8254b60aa60364a8cc53e19910f88050ca1
nezzyyoung <nezzybucumi@gmail.com>
2025-06-09 22:55:45 +0300

    updates

---
+++ src/app/resources/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+export default function ResourcesPage() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold mb-6 text-gray-900 dark:text-white">Resources</h1>
+      <p className="text-gray-700 dark:text-gray-300">
+        Welcome to the Resources page. Here you will find useful materials and links to help you get started.
+      </p>
+    </main>
+  );
+}
