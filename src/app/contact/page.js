27ebab7a10d02f89707f10e4c5a4985d8276ada1
nezzyyoung <nezzybucumi@gmail.com>
2025-06-09 22:55:32 +0300

    updates

---
+++ src/app/contact/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold mb-6 text-gray-900 dark:text-white">Contact</h1>
+      <p className="text-gray-700 dark:text-gray-300">
+        Get in touch with us through this Contact page. We would love to hear from you!
+      </p>
+    </main>
+  );
+}
